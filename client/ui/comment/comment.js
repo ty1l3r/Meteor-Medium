90d7207001ec7b73d42ed7b716b949a3e0c9ce27
tyler <h4kh4@outlook.fr>
2019-04-08 10:57:24 +0200

    + Commentaires OP

---
+++ client/ui/comment/comment.js
@@ -0,0 +1,30 @@
+import './comment.html';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import { Comments } from '../../../both';
+
+Template.comment_form.events({
+    'submit .js-create-comment'(event, instance) {
+        event.preventDefault();
+
+        const content = event.target.content.value;
+
+        let commentDoc = {
+            content: content, 
+            articleId: FlowRouter.getParam('articleId'),
+            createdAt: new Date(),
+            ownerId: Meteor.userId()
+        }
+
+        Comments.insert(commentDoc);
+
+        event.target.content.value = ''; // vide le champ
+    }
+});
+
+// affichage des articles avec l'Id Correspondante
+
+Template.comment_list.helpers({
+    comments() {
+        return Comments.find({ articleId: FlowRouter.getParam('articleId') });
+    }
+});
--- client/ui/layout/layout.js
+++ client/ui/layout/layout.js
@@ -1,4 +1,5 @@
 import './layout.html';
 import '../navbar/navbar'; //prend en compte les ".js"
 import '../home/home';
+import '../comment/comment';
 import '../article/article';
