55b68b03297db26a5b4c2a15b6faab712c02bce6
ty1l3r <ak-vitrail@live.fr>
2019-05-30 18:45:13 +0200

    Site opérationel (Sécurité + CRUD complet)

--- both/accountConfig.js
+++ both/accountConfig.js
@@ -4,6 +4,15 @@ T9n.setLanguage('fr');
 let email = AccountsTemplates.removeField('email');
 let password = AccountsTemplates.removeField('password');
 
+AccountsTemplates.configure({
+    // Behavior
+    enablePasswordChange: true,
+     // Appearance
+    showForgotPasswordLink: true,
+      // Privacy Policy and Terms of Use
+    privacyUrl: 'privacy',
+});
+
 AccountsTemplates.addField({
     _id: 'pseudo',
     type: 'text',
@@ -17,8 +26,8 @@ AccountsTemplates.addField({
 AccountsTemplates.addField({
     _id: 'firstname',
     type: 'text',
-    displayName: 'Firstname',
-    placeholder: 'Firstname',
+    displayName: 'Prénom',
+    placeholder: 'Prénom',
     required: true,
     minLenght: 3,
     trim: true
@@ -27,14 +36,17 @@ AccountsTemplates.addField({
 AccountsTemplates.addField({
     _id: 'name',
     type: 'text',
-    displayName: 'Name',
-    placeholder: 'Name',
+    displayName: 'Nom',
+    placeholder: 'Nom',
     required: true,
     minLenght: 3,
     trim: true
 });
 
-password.minLenght = 3;
+password.minLenght = 6;
+
 
 AccountsTemplates.addField(email);
+
+
 AccountsTemplates.addField(password);
